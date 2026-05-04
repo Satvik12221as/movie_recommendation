36a16ac813202cbafbdbfa683e35601482ae3428
satvik <sanu8800971634@gmail.com>
2026-05-04 23:54:08 +0530

    fix: automated fix for issue #3 - task.py

--- task.py
+++ task.py
@@ -5,16 +5,33 @@ credits = pd.read_csv("tmdb_5000_credits.csv")
 movies = movies.merge(credits, on="title")
 
 
-movies = movies[["movie_id", "title", "overview"]]
-movies.dropna(inplace=True)                                                # using dropna to remove missing values(NAN)
+movies = movies[["movie_id", "title", "overview", "genres", "keywords", "cast"]]
 
 
 from sklearn.feature_extraction.text import TfidfVectorizer
-tfidf = TfidfVectorizer(stop_words="english")                              #Changing the overview into vectors
-vectors = tfidf.fit_transform(movies["overview"])
+from gensim.models import Word2Vec
+import numpy as np
+
+def combine_features(row):
+    return row["overview"] + " " + row["genres"] + " " + row["keywords"] + " " + row["cast"]
+
+movies["combined_features"] = movies.apply(combine_features, axis=1)
+
+sentences = [row.split() for row in movies["combined_features"]]
+model = Word2Vec(sentences, vector_size=100, min_count=1)
+
+def get_vector(row):
+    words = row.split()
+    vector = np.zeros(100)
+    for word in words:
+        if word in model.wv:
+            vector += model.wv[word]
+    return vector / len(words)
+
+movies["vector"] = movies["combined_features"].apply(get_vector)
 
 
-from sklearn.metrics.pairwise import cosine_similarity
+similarity = cosine_similarity(movies["vector"].tolist())
 similarity = cosine_similarity(vectors)
 
 def recommend(movie_name):
@@ -29,4 +46,6 @@ def recommend(movie_name):
 recommend("Spectre")
 print("These are the similar movies to Spectre")
 recommend("The Lego Movie")
+print("These are the similar movies to The Lego Movie")
+print("These are the similar movies to Spectre")
 print("These are the similar movies to The Lego Movie")
